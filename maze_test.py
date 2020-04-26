ca7ef5fd4ca72d742b8d42e04c32fadef882385d
Matheus Esteves <matheusesteveszanoto@gmail.com>
2020-04-26 17:35:36 -0300

    Improve - MAZE_1, MAZE_2, MAZE_3, MAZE_4 and MAZE_5

--- maze_test.py
+++ maze_test.py
@@ -1,4 +1,10 @@
-from maze_generator.generator import MAZE_3
+from maze_generator.generator import MAZE_1, MAZE_2, MAZE_3, MAZE_4, MAZE_5
 
 if __name__ == '__main__':
+  '''
+  MAZE_1.show_search_path([],[])
+  MAZE_2.show_search_path([],[])
   MAZE_3.show_search_path([],[])
+  MAZE_4.show_search_path([],[])
+  '''
+  MAZE_5.show_search_path([],[])
