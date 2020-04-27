8e604f2ff73c789144e6d9a5c85b7256e4939388
Felipe Rodrigues <felipe@felipevr.com>
2020-04-27 17:18:05 -0300

    Add poetry as project manager

--- pacman_problem.py
+++ pacman_problem.py
@@ -1,10 +1,10 @@
 from random import shuffle
 import math
 
-import problem
+from problem import Problem
 import maze
 
-class PacManProblem(Problem):    
+class PacManProblem(Problem):
     index_by = lambda ij, m: m[ij[0]][ij[1]]
 
     """
@@ -34,19 +34,19 @@ class PacManProblem(Problem):
 
     def __is_valid_pos(self, pos):
         i, j = pos
-        return  0 <= i and i < self.height and 
+        return  (0 <= i and i < self.height and
                 0 <= j and j < self.width and
-                self.maze[i][j] != maze.GHOST and 
-                self.maze[i][j] != maze.WALL
+                self.maze[i][j] != maze.GHOST and
+                self.maze[i][j] != maze.WALL)
 
     def __is_valid_move(self, from_pos, to_pos):
         from_i, from_j = from_pos
         to_i, to_j = to_pos
 
         delta = abs((from_i-to_i) + (from_j-to_j))
-        return self.__is_valid_pos(from_pos) and 
+        return (self.__is_valid_pos(from_pos) and
                self.__is_valid_pos(to_pos) and
-               delta <= 1
+               delta <= 1)
 
     """
     Parameters
@@ -58,10 +58,10 @@ class PacManProblem(Problem):
 
         i, j = state
         actions_list = [(i + di, j + dj) for (di, dj) in directions]
-        
+
         if (self.shuffle_actions_list):
             shuffle(actions_list) # randomizes actions' order
-        
+
         return [pos for pos in actions_list if self.__is_valid_move(state, pos)]
 
     """
@@ -93,8 +93,8 @@ class PacManProblem(Problem):
       next_pos : (int, int)
         pacman's next postition at the maze
     """
-    def path_cost(self, cost_so_far, current_pos, action, next_pos):                
-        if action == next_pos and self.__valid_move(current_pos, next_pos):            
+    def path_cost(self, cost_so_far, current_pos, action, next_pos):
+        if action == next_pos and self.__valid_move(current_pos, next_pos):
             return cost_so_far + 1
 
         return math.inf
@@ -105,24 +105,24 @@ class PacManProblem(Problem):
         pacman's current postition at the maze
         for which we estimate the lowest path cost to the goal
     """
-    def h(self, state):        
-        return manhattan(state, goal)   
+    def h(self, state):
+        return manhattan(state, goal)
 
     """
     Parameters
       state : (int, int)
         pacman's current postition at the maze
-    
+
     This acts as a bit of extra information in problems where we try to
     optimise a value when we cannot do a goal test. Used in Hill-climbing algorithm.
     """
     def value(self, state):
         i, j = state
-        
+
         if self.maze[i][j] == maze.FREE:
             return 1
-        
+
         if self.maze[i][j] == maze.COIN:
             return 2
-        
+
         return -math.inf
